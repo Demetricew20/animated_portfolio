4cb5046f9bee24600adaadf2af1e670124fe689f
DemetriceW20 <demetrice_williams@hotmail.com>
2023-12-22 11:36:30 -0600

    add constants. add navbar. updated tailwind config with animation.

--- components/main/Hero.tsx
+++ components/main/Hero.tsx
@@ -5,7 +5,7 @@ import HeroContent from '../sub/HeroContent'
 const Hero = () => {
   return (
     <div className='relative flex flex-col h-full w-full'>
-      <video autoPlay muted loop className='rotate-180 absolute top-[-340px] lef-0 z-[1] w-full h-full object-cover'>
+      <video autoPlay muted loop className='rotate-180 absolute top-[-340px] lef-0 z-[1] w-full h-full'>
         <source src="/blackhole.webm" type='video/webm'></source>
     </video>
     <HeroContent />
